ac8466cf64248a1331914dabd498d7309df61f8f
Moritz Schramm <mail@mo5.ch>
2025-11-20 20:11:07 +0100

    add name to image_loader arguments

--- include/image_loader.h
+++ include/image_loader.h
@@ -3,5 +3,5 @@
 #include <filesystem>
 #include <opencv2/opencv.hpp>
 
-std::vector<std::filesystem::path> find_image_files_recursively(const std::filesystem::path&, const std::string&);
+std::vector<std::filesystem::path> find_image_files_recursively(const std::filesystem::path& dir, const std::string& image_format);
 cv::Mat load_image(const std::filesystem::path& path);
