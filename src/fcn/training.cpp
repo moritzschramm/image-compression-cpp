1088c715022859e9decd9b8d4a61928c172c51d1
Moritz Schramm <mail@mo5.ch>
2025-11-27 20:07:17 +0100

    add boilerplate code for training

--- src/fcn/training.cpp
+++ src/fcn/training.cpp
@@ -1,6 +1,19 @@
+#include <c10/core/DeviceType.h>
+#include <torch/torch.h>
+#include "fcn/EdgeUNet.h"
 
 int main()
 {
-    // TODO
+    const auto device = torch::kCUDA;
+
+    EdgeUNet model;
+    torch::load(model, "fcn_pretrained.pt");
+
+    model->to(device);
+
+    // TODO training loop where segment-level scores are used to create edge supervision signals
+
+    torch::save(model, "fcn_" + std::to_string(std::time(0)) + ".pt");
+
     return 0;
 }
