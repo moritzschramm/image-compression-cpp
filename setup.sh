d006fc39206e202f6a1c820f75b47e4faa8c0ff8
Moritz Schramm <mail@mo5.ch>
2025-12-14 16:30:55 +0100

    add patch for RAMA in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 mkdir dataset
 mkdir results
+git submodule update --init --recursive
+
+# custom patch to make RAMA compile
+sed -i 's/include_directories(external/ECL-CC)/include_directories(${CMAKE_CURRENT_SOURCE_DIR}external/ECL-CC)/g' external/RAMA/CMakeLists.txt
+sed -i 's/include_directories(include)/include_directories(${CMAKE_CURRENT_SOURCE_DIR}include)/g' external/RAMA/CMakeLists.txt
+sed -i '/enable_testing()/d' external/RAMA/CMakeLists.txt
+sed -i '/add_subdirectory(test)/d' external/RAMA/CMakeLists.txt
+sed -i '/pybind11_add_module(rama_py rama_py.cu)/d' external/RAMA/src/CMakeLists.txt
+sed -i '/target_link_libraries(rama_py PRIVATE multicut_text_parser rama_cuda RAMA)/d' external/RAMA/src/CMakeLists.txt
+
+
 cmake -B build -S .
